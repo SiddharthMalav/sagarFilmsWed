b1868a67bbbd079c337dfac26a079caa7f9358b9
SiddharthMalav <89537562+SiddharthMalav@users.noreply.github.com>
2024-12-23 13:42:27 +0530

    images update

--- src/components/whatWeProvide.tsx
+++ src/components/whatWeProvide.tsx
@@ -6,16 +6,16 @@ export default function WhatWeProvide() {
       <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">What We Provide</h2>
       <div className="font-serif mt-4 grid grid-cols-3 gap-4 sm:gap-6 lg:gap-8">
         <div>
-          <img src="/images/0I1A9668_11zon.jpg" className="rounded-lg bg-gray-100" />
+          <img src="/images/730A0138 copy_11zon.jpg" className="rounded-lg bg-gray-100" />
           <h2 className="mt-2 text-center text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">Golden Hour Vows
           </h2>
         </div>
         <div>
-          <img src="/images/IMG_9406_11zon.jpg" className="rounded-lg bg-gray-100" />
+          <img src="/images/pre-wedding/730A4377 copy_11zon.jpg" className="rounded-lg bg-gray-100" />
           <h2 className="mt-2 text-center text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">Journey to Forever</h2>
         </div>
         <div>
-          <img src="/images/IMG_9401_11zon.jpg" className="rounded-lg bg-gray-100" />
+          <img src="/images/pre-wedding/a16_11zon (1).jpg" className="rounded-lg bg-gray-100" />
           <h2 className="mt-2 text-center text-xl font-semibold tracking-tight text-gray-900 sm:text-2xl">Candid Charm</h2>
         </div> </div>
     </div>)
