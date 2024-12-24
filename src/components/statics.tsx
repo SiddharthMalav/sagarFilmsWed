4cf8efee0d0c59b9e058f58a1a0f2bbbd5d56986
SiddharthMalav <89537562+SiddharthMalav@users.noreply.github.com>
2024-12-24 20:13:16 +0530

    padding adding

--- src/components/statics.tsx
+++ src/components/statics.tsx
@@ -6,8 +6,8 @@ export const CompanyStatics = () => {
     <section className='relative py-8 md:py-10 bg-gradient-to-r from-[#000099] via-[#5a5acc] to-[#cc33ff]'>
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-      <section className='relative z-10'> <h2 className="text-white text-center text-2xl font-semibold tracking-tight  sm:text-3xl">Our Remarkable Achievements to Share</h2>
-        <p className="text-center text-white">we take pride in showcasing our accomplishments that reflect our growth and success.<br /> Explore the highlights of our journey and the impact we’ve made so far! </p>
+      <section className='relative z-10'> <h2 className="px-4 text-white text-center text-2xl font-semibold tracking-tight  sm:text-3xl">Our Remarkable Achievements to Share</h2>
+        <p className="text-center px-4 text-white">we take pride in showcasing our accomplishments that reflect our growth and success.<br /> Explore the highlights of our journey and the impact we’ve made so far! </p>
 
         <section className="flex lg:flex-row flex-col max-md:text-3xl text-4xl text-white md:gap-20 gap-10 items-center justify-center px-6 pt-4 md:pt-6">
           <div className="flex flex-col justify-center">
